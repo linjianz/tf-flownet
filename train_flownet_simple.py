95c262a41e9e9791546cfbd461500b99742c9a5e
linjian93 <1739102936@qq.com>
2017-06-24 15:37:09 +0800

    add record file

--- train_flownet_simple.py
+++ train_flownet_simple.py
@@ -10,32 +10,32 @@ import random
 import os
 import cv2
 import numpy as np
-import scipy.io as sio
 import shutil
+import struct
+from datetime import datetime
 
 ########################################
 # configuration
-
 lr_base = 1e-3
-epoch_max = 20
+epoch_max = 10
 epoch_lr_decay = 500
 epoch_save = 2
 max_to_keep = 5
-
-batch_size = 4
+batch_size = 64
+train_pairs_number = 20000
+iter_per_epoch = train_pairs_number // batch_size
 width = 512
 height = 384
 
-dir0 = '20170623'  # change it every time when training
-net_name = 'flownet/'
-# dir_restore = ''
+dir0 = '20170624_1'  # change it every time when training
+net_name = 'flownet_simple/'
 
 dir_models = 'model/' + net_name
 dir_logs = 'log/' + net_name
 dir_model = dir_models + dir0
 dir_log_train = dir_logs + dir0 + '_train'
 dir_log_test = dir_logs + dir0 + '_test'
-
+dir_dataset = '/media/csc105/Data/dataset/FlyingChairs/data/'
 if not os.path.exists(dir_models):
     os.mkdir(dir_models)
 if not os.path.exists(dir_logs):
@@ -53,60 +53,102 @@ os.mkdir(dir_log_test)
 
 ########################################
 # load image mean
-
-dir_mean = 'data/mean_16338.mat'
-mean_load = sio.loadmat(dir_mean)
-mean = mean_load['mean']
+# dir_mean = 'data/mean.mat'
+# mean_load = sio.loadmat(dir_mean)
+# mean = mean_load['mean']
 
 
 ########################################
 # data process
-
 def removeFile(directory_list):
     if '.directory' in directory_list:
         directory_list.remove('.directory')
     return directory_list
 
 
-def load_data_train():
+def load_data():
+    img1_list_t = []  # [1, 2, 3, ..., 20000]
+    img2_list_t = []
+    flow_list_t = []
+    img1_list_v = []  # [1, 2, 3, ..., 2000]
+    img2_list_v = []
+    flow_list_v = []
+    namelist = removeFile(os.listdir(dir_dataset))
+    namelist.sort()
+    for i in range(train_pairs_number+2000):
+        if i < train_pairs_number:
+            flow_list_t.append(dir_dataset + namelist[3*i])
+            img1_list_t.append(dir_dataset + namelist[3*i+1])
+            img2_list_t.append(dir_dataset + namelist[3*i+2])
+        else:
+            flow_list_v.append(dir_dataset + namelist[3*i])
+            img1_list_v.append(dir_dataset + namelist[3*i+1])
+            img2_list_v.append(dir_dataset + namelist[3*i+2])
 
-    return 0
+    assert len(img1_list_t) == len(img2_list_t)
+    assert len(img1_list_t) == len(flow_list_t)
+    assert len(img1_list_v) == len(img2_list_v)
+    assert len(img1_list_v) == len(flow_list_v)
+    return img1_list_t, img2_list_t, flow_list_t, img1_list_v, img2_list_v, flow_list_v
 
 
-def load_data_val():
-
-    return 0
-
-
-class DataBatch(object):
-    def __init__(self, list, index, label, bs=batch_size, shuffle=True, minus_mean=True):
-        self.list = list
-        self.start_index = index  # [0, 5, 10, ...]
-        self.label = np.array(label)
+class Data(object):
+    def __init__(self, list1, list2, list3, bs=batch_size, shuffle=True, minus_mean=True):
+        self.list1 = list1
+        self.list2 = list2
+        self.list3 = list3
         self.bs = bs
         self.index = 0
-        self.number = len(self.start_index)  # ~16330/5
+        self.number = len(self.list1)
+        self.index_total = range(self.number)
         self.shuffle = shuffle
         self.minus_mean = minus_mean
-        if self.shuffle: random.shuffle(self.start_index)
+        if self.shuffle: random.shuffle(self.index_total)
+
+    def read_flow(self, name):
+        f = open(name, "rb")
+        data = f.read()
+        f.close()
+        width = struct.unpack('@i', data[4:8])[0]
+        height = struct.unpack('@i', data[8:12])[0]
+        flowdata = np.zeros((height, width, 2))
+        for i in range(width*height):
+            data_u = struct.unpack('@f', data[12+8*i:16+8*i])[0]
+            data_v = struct.unpack('@f', data[16+8*i:20+8*i])[0]
+            n = int(i / width)
+            k = np.mod(i, width)
+            flowdata[n, k, :] = [data_u, data_v]
+        return flowdata
 
     def next_batch(self):
         start = self.index
         self.index += self.bs
         if self.index > self.number:
-            if self.shuffle: random.shuffle(self.start_index)
+            if self.shuffle: random.shuffle(self.index_total)
             self.index = 0
             start = self.index
             self.index += self.bs
         end = self.index
+        img1_batch = []
+        img2_batch = []
+        flow_batch = []
+        for i in range(start, end):
+            img1 = cv2.imread(self.list1[self.index_total[i]])
+            img1_batch.append(img1)
+            img2 = cv2.imread(self.list2[self.index_total[i]])
+            img2_batch.append(img2)
+            flow = self.read_flow(self.list3[self.index_total[i]])
+            flow_batch.append(flow)
+
+        return img1_batch, img2_batch, flow_batch
 
 
 ########################################
-class Net_Model(object):
+class NetModel(object):
     def __init__(self, constraints=False):
         self.x1 = tf.placeholder(tf.float32, [None, height, width, 3], name='x1')  # image1
         self.x2 = tf.placeholder(tf.float32, [None, height, width, 3], name='x2')  # image2
-        self.x3 = tf.placeholder(tf.float32, [None, height, width, 2], name='x2')  # label
+        self.x3 = tf.placeholder(tf.float32, [None, height, width, 2], name='x3')  # label
         self.x4 = tf.placeholder(tf.float32, [], name='x4')  # lr
         with tf.variable_scope('conv'):
             concat1 = tf.concat(3, [self.x1, self.x2])
@@ -119,12 +161,12 @@ class Net_Model(object):
             conv5 = slim.conv2d(conv4_1, 512, [3, 3], 2, scope='conv5')
             conv5_1 = slim.conv2d(conv5, 512, [3, 3], 1, scope='conv5_1')
             conv6 = slim.conv2d(conv5_1, 1024, [3, 3], 2, scope='conv6')
-            conv6_1 = slim.conv2d(conv5_1, 1024, [3, 3], 1, scope='conv6_1')
+            conv6_1 = slim.conv2d(conv6, 1024, [3, 3], 1, scope='conv6_1')
             predict6 = slim.conv2d(conv6_1, 2, [3, 3], 1, scope='pred6')
 
         with tf.variable_scope('deconv'):
             # 12 * 16 flow
-            deconv5 = slim.conv2d_transpose(conv6, 512, [4, 4], 2, scope='deconv5')
+            deconv5 = slim.conv2d_transpose(conv6_1, 512, [4, 4], 2, scope='deconv5')
             deconvflow6 = slim.conv2d_transpose(predict6, 2, [4, 4], 2, 'SAME', scope='deconvflow6')
             concat5 = tf.concat(3, [conv5_1, deconv5, deconvflow6], name='concat5')
             predict5 = slim.conv2d(concat5, 2, [3, 3], 1, 'SAME', scope='predict5')
@@ -183,30 +225,26 @@ class Net_Model(object):
 # main function
 def main(_):
     # data preparation
-    list, index, label = load_data_train()
-    dataset_train = DataBatch(list, index, label, shuffle=True, minus_mean=True)
-    list_v, index_v, label_v = load_data_val()
-    dataset_val = DataBatch(list_v, index_v, label_v, shuffle=True, minus_mean=True)
-    x1_v, x2_v, x1_1_v, x2_1_v, x3_v = dataset_val.next_batch()
+    list1_t, list2_t, list3_t, list1_v, list2_v, list3_v = load_data()
+    dataset_t = Data(list1_t, list2_t, list3_t, shuffle=True, minus_mean=False)
+    dataset_v = Data(list1_v, list2_v, list3_v, shuffle=True, minus_mean=False)
+    x1_v, x2_v, x3_v = dataset_v.next_batch()
 
-    model = Net_Model()
+    model = NetModel()
     with tf.Session(config=model.tf_config) as sess:
         sess.run(model.init)
         writer_train = tf.train.SummaryWriter(dir_log_train, sess.graph)
         writer_val = tf.train.SummaryWriter(dir_log_test, sess.graph)
-
         for epoch in xrange(epoch_max):
-            iter_max = len(index) // batch_size
             lr_decay = 0.1 ** (epoch / epoch_lr_decay)
             lr = lr_base * lr_decay
-            for iter in xrange(iter_max):
-                global_iter = epoch * iter_max + iter
-                x1_batch, x2_batch, x3_batch = dataset_train.next_batch()
-
+            for iter in xrange(iter_per_epoch):
+                global_iter = epoch * iter_per_epoch + iter
+                x1_t, x2_t, x3_t = dataset_t.next_batch()
                 feed_dict = {}
-                feed_dict[model.x1] = x1_batch
-                feed_dict[model.x2] = x2_batch
-                feed_dict[model.x3] = x3_batch
+                feed_dict[model.x1] = x1_t
+                feed_dict[model.x2] = x2_t
+                feed_dict[model.x3] = x3_t
                 feed_dict[model.x4] = lr
                 sess.run(model.train_op, feed_dict)
 
@@ -214,8 +252,7 @@ def main(_):
                 if not (iter + 1) % 1:
                     merged_out_t, loss_out_t = sess.run([model.merged, model.loss], feed_dict)
                     writer_train.add_summary(merged_out_t, global_iter + 1)
-                    print('epoch %03d, iter %04d, lr %.6f, loss: %.5f' % (epoch + 1, iter + 1, lr, loss_out_t))
-
+                    print('%s, epoch %03d, iter %04d, lr %.5f, loss: %.5f' % (datetime.now(), epoch + 1, iter + 1, lr, loss_out_t))
                 if not (iter + 1) % 5:
                     feed_dict_v = {}
                     feed_dict_v[model.x1] = x1_v
@@ -223,11 +260,11 @@ def main(_):
                     feed_dict_v[model.x3] = x3_v
                     merged_out_v, loss_out_v = sess.run([model.merged, model.loss], feed_dict_v)
                     writer_val.add_summary(merged_out_v, global_iter + 1)
-                    print('epoch %03d, iter %04d, ****val loss****: %.5f' % (epoch + 1, iter + 1, loss_out_v))
+                    print('%s, epoch %03d, iter %04d, ****val loss****: %.5f' % (datetime.now(), epoch + 1, iter + 1, loss_out_v))
 
                 # save
-                if not (global_iter + 1) % (epoch_save * iter_max):
-                    model.saver.save(sess, (dir_model + '/model'), global_step=global_iter + 1)
+                if not (global_iter + 1) % (epoch_save * iter_per_epoch):
+                    model.saver.save(sess, (dir_model + '/model'), global_step=global_iter+1)
 
 
 if __name__ == "__main__":
